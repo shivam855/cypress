513728f1028aa4f0798850e5dae99b56c24ea4ee
Shivam Rathour <66547505+shivam855@users.noreply.github.com>
2023-01-15 10:27:48 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    url: "https://google.com/",
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+        return require('./cypress/plugins/index.js')(on, config);
+    },
+    specPattern: "**/*.feature",
+  },
+});
