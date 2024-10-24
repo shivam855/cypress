a90157ec1543e5a136b48d7ad68afe1e3251871e
Shivam Rathour <66547505+shivam855@users.noreply.github.com>
2024-10-24 17:47:37 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,49 @@
 const { defineConfig } = require("cypress");
+const {
+  beforeRunHook,
+  afterRunHook,
+} = require("cypress-mochawesome-reporter/lib");
+// const TestRailReporter = require("cypress-testrail");
 
 module.exports = defineConfig({
-  // env: {
-  //   url: "https://google.com/",
-  // },
-  
+  video: true,
+  experimentalStudio: true,
+  experimentalInteractiveRunEvents: true,
+  chromeWebSecurity: true,
+  trashAssetsBeforeRuns: true,
+  pageLoadTimeout: 120000,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true,
+  },
+  env: {
+    CYPRESS_ENV: "DEV",
+    CYPRESS_USER: "admin",
+    CYPRESS_PASSWORD: "admin",
+    QA_BASE_URL: "https://admin-qa.simpleonlinehealthcare.co.uk/",
+    DEV_BASE_URL: "https://admin-dev.simpleonlinehealthcare.co.uk/",
+    PROD_BASE_URL: "https://simpleonlinehealthcare.co.uk/",
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
-        return require('./cypress/plugins/index.js')(on, config);
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+      });
+
+      on("after:run", async () => {
+        console.log("override after:run");
+        await afterRunHook();
+      });
+      require("cypress-mochawesome-reporter/plugin")(on);
+
+      // new TestRailReporter(on, config).register();
+      // return config;
     },
-    specPattern: "./cypress/integration/*.js",
   },
 });
